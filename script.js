c2405fbd8a4ce6e7b0cd5ae58e731dee7ced2837
Jake M <J.Mikulin@sms.ed.ac.uk>
2021-03-06 18:33:08 +0000

    Webpage update

---
+++ script.js
@@ -0,0 +1,20 @@
+let goodboybutton = document.querySelector('#goodboybutton');
+let repeatbutton = document.querySelector('#repeatbutton');
+
+let dogbox = document.querySelector('#dogbox');
+
+goodboybutton.addEventListener('click', () => {
+    document.getElementById("dogbox").style.backgroundImage = "url('images/after-good-boy.jpg')";
+});
+
+repeatbutton.addEventListener('click', () => {
+    document.getElementById("dogbox").style.backgroundImage = "url('images/before-good-boy.jpg')";
+});
+
+dogbox.addEventListener('mouseover', () => {
+    document.getElementById("dogbox").style.backgroundImage = "url('images/after-good-boy.jpg')";
+});
+
+dogbox.addEventListener('mouseleave', () => {
+    document.getElementById("dogbox").style.backgroundImage = "url('images/before-good-boy.jpg')";
+});
